6e643d90c19c98e95f8b9b9d1f12c581ffe81db3
ThisIsMeWinning <daltoncclinton@gmail.com>
2021-03-01 00:22:43 -0600

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net.NetworkInformation;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        public static void PingHost(string nameOrAddress)
+        {
+            bool pingable = false;
+            Ping pinger = null;
+            pinger = new Ping();
+            bool x = true;
+            while (x == true)
+            {
+                PingReply reply = pinger.Send(nameOrAddress);
+                pingable = reply.Status == IPStatus.Success;
+                if (pingable == true)
+                {
+                    Console.WriteLine(nameOrAddress + " is replying");
+                }
+                else
+                {
+                    Console.WriteLine(nameOrAddress + " is not replying");
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            PingHost("192.168.1.24");
+        }
+    }
+}
